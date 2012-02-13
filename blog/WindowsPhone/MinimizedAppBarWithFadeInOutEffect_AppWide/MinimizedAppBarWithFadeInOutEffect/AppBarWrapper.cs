4e4484913021f432012e540c8d491219c34c3fee
Patrick Finnigan <patrick.k.finnigan@gmail.com>
2012-02-12 16:10:29 -0800

    MinimizedAppBarWithFadeInOutEffect_AppWide

---
+++ blog/WindowsPhone/MinimizedAppBarWithFadeInOutEffect_AppWide/MinimizedAppBarWithFadeInOutEffect/AppBarWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Shell;
+
+namespace MinimizedAppBarWithFadeInOutEffect
+{
+    public class AppBarWrapper : DependencyObject
+    {
+        public double Opacity
+        {
+            get { return (double)GetValue(OpacityProperty); }
+            set { SetValue(OpacityProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Opacity.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OpacityProperty =
+            DependencyProperty.Register("Opacity", typeof(double), typeof(AppBarWrapper),
+                new PropertyMetadata(App.Current.Resources["AppBarSemitransparentOpacity"],
+                    new PropertyChangedCallback((o, a) => ((ApplicationBar)App.Current.Resources["MyAppBar"]).Opacity = (double)a.NewValue )));
+    }
+}
---
+++ blog/WindowsPhone/MinimizedAppBarWithFadeInOutEffect_AppWide/MinimizedAppBarWithFadeInOutEffect/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace MinimizedAppBarWithFadeInOutEffect
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+        }
+    }
+}
