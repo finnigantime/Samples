ba3dcf45b88832f01c38486b40d6632df3673129
Patrick Finnigan <patrick.k.finnigan@gmail.com>
2013-08-10 13:32:29 -0700

    Adding GridViewItemLoadedUnloadedAnimations sample

---
+++ examples/Win8Xaml/GridViewItemLoadedUnloadedAnimations/GridViewItemLoadedUnloadedAnimations/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace GridViewItemLoadedUnloadedAnimations
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ObservableCollection<int> myInts = new ObservableCollection<int>();
+            for (int i = 0; i < 10; ++i)
+                myInts.Add(i);
+
+            MyGV.ItemsSource = myInts;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Storyboard sb = ((Grid)sender).Resources["LoadedStoryboard"] as Storyboard;
+            sb.Begin();
+        }
+    }
+}
