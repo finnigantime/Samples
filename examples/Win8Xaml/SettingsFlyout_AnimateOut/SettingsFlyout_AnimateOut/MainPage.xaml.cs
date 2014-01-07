eb701aec50e77cfd20691d29b1fb16db5e118d43
Patrick Finnigan <patrick.k.finnigan@gmail.com>
2014-01-07 11:49:34 -0800

    Adding SettingsFlyout_AnimateOut example

---
+++ examples/Win8Xaml/SettingsFlyout_AnimateOut/SettingsFlyout_AnimateOut/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SettingsFlyout_AnimateOut
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Windows.UI.ApplicationSettings.SettingsPane.GetForCurrentView().CommandsRequested += MainPage_CommandsRequested;
+        }
+
+
+    void MainPage_CommandsRequested(Windows.UI.ApplicationSettings.SettingsPane sender, Windows.UI.ApplicationSettings.SettingsPaneCommandsRequestedEventArgs args)
+    {
+        Windows.UI.ApplicationSettings.SettingsCommand updateSetting = 
+            new Windows.UI.ApplicationSettings.SettingsCommand("AppUpdateSettings", "App updates", (handler) =>
+        {
+            SettingsFlyout1 updatesFlyout = new SettingsFlyout1();
+            updatesFlyout.ShowCustom();
+
+        });
+
+        args.Request.ApplicationCommands.Add(updateSetting);
+    }
+
+    }
+}
---
+++ examples/Win8Xaml/SettingsFlyout_AnimateOut/SettingsFlyout_AnimateOut/SettingsFlyout1.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ApplicationSettings;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Settings Flyout item template is documented at http://go.microsoft.com/fwlink/?LinkId=273769
+
+namespace SettingsFlyout_AnimateOut
+{
+    public sealed partial class SettingsFlyout1 : SettingsFlyout
+    {
+        Popup _p;
+        
+        public SettingsFlyout1()
+        {
+            this.InitializeComponent();
+
+            BackClick += SettingsFlyout1_BackClick;
+            Unloaded += SettingsFlyout1_Unloaded;
+        }
+
+        void SettingsFlyout1_BackClick(object sender, BackClickEventArgs e)
+        {
+            SettingsPane.Show();
+        }
+
+        void SettingsFlyout1_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (_p != null)
+            {
+                _p.IsOpen = false;
+            }
+        }
+
+        public void ShowCustom()
+        {
+            _p = new Popup();
+            Border b = new Border();
+
+            b.ChildTransitions = new TransitionCollection();
+
+            // TODO: if you support right-to-left builds, make sure to test all combinations of RTL operating
+            // system build (charms on left) and RTL flow direction for XAML app.  EdgeTransitionLocation.Left
+            // may need to be used for RTL (and HorizontalAlignment.Left on the SettingsFlyout below).
+            b.ChildTransitions.Add(new EdgeUIThemeTransition() { Edge = EdgeTransitionLocation.Right });
+
+            b.Background = new SolidColorBrush(Colors.Transparent);
+            b.Width = Window.Current.Bounds.Width;
+            b.Height = Window.Current.Bounds.Height;
+            b.Tapped += b_Tapped;
+
+            this.HorizontalAlignment = HorizontalAlignment.Right;
+            b.Child = this;
+
+            _p.Child = b;
+            _p.IsOpen = true;
+        }
+
+        void b_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Border b = (Border)sender;
+            b.Child = null;
+        }
+    }
+}
